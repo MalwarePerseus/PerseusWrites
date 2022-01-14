e3650f571956e40825aaf623817a1a3c3ee3af4d
Deepak Singh <malwareperseus@gmail.com>
2022-01-15 03:40:02 +0530

    Navigation with dummy categories

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useContext} from 'react'
+import Link from 'next/link'
 
+const categories = [
+    {name: 'Web Development', slug: 'webdev'},
+    {name: 'Game Development', slug: 'gamedev'}
+]
 function Header() {
     return (
-        <div>
-            
+        <div className="container mx-auto px-10 mb-8">
+            <div className="border-b w-full inline-block border-gray-400 py-8">
+                <div className="md:float-left block">
+                    <Link href="/">
+                        <span className='cursor-pointer font-bold text-4xl text-gray-700'>
+                            Perseus Writes
+                        </span>
+                    </Link>
+                </div>  
+                <div className='hidden md:float-left md:contents'>
+                    {categories.map((category) => (
+                        <Link key={category.slug} href={`/category/${category.slug}`}>
+                            <span className='md:float-right mt-2 align-middle text-gray-650 ml-4'>
+                                {category.name}
+                            </span>
+                        </Link>
+                    ))}
+                </div>  
+            </div>
         </div>
     )
 }
