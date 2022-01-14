5f92106ff84f9fc3bcf814f772f956a0d3eb54a8
Deepak Singh <malwareperseus@gmail.com>
2022-01-15 01:49:08 +0530

    Main Page Layout Structre

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,26 @@ const posts =  [
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="bg-purple-300 container mx-auto px-10 mb-8">
       <Head>
         <title>Perseus Writes</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post, index) => (
+              <div>
+                {post.title}
+                {post.excerpt}
+              </div>
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
 
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
